1cfeb240098ea8bdffd87dac00b90a42f2b5611f
Necip Sunmaz <necipsunmaz@msn.com>
2017-04-11 22:02:08 +0300

    Add history map slider

--- HistoryMap/Controllers/HomeController.cs
+++ HistoryMap/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
 using HistoryMap.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HistoryMap.Controllers
 {
@@ -28,18 +29,25 @@ namespace HistoryMap.Controllers
         }
 
         [HttpPost]
-        public IActionResult GetCountryDetails(string country)
+        public IActionResult GetCountryDetails(string country, int id)
         {
-            var Country = db.Countries.FirstOrDefault(a => a.Title.Equals(country));
+            var Country = db.Countries.FirstOrDefault(w => w.Title.Equals(country) && w.Century.Century.Equals(id));
             return Json(Country);
         }
 
         public IActionResult GetData(int Centurie)
         {
-            var centuryMap = db.Centuries.FirstOrDefault(a => a.Century.Equals(Centurie));
-            MemoryStream file = new MemoryStream(centuryMap.Data);
-            //binaryData
-            return new FileStreamResult(file, "application/javascript");
+            try
+            {
+                var centuryMap = db.Centuries.FirstOrDefault(a => a.Century.Equals(Centurie));
+                MemoryStream file = new MemoryStream(centuryMap.Data);
+                //binaryData
+                return new FileStreamResult(file, "application/javascript");
+            }
+            catch (Exception)
+            {
+                return Content("alert('Henüz bu yüzyılı eklemedik!');");
+            }
         }
 
     }
--- HistoryMap/wwwroot/js/site.js
+++ HistoryMap/wwwroot/js/site.js
@@ -1,1 +1,113 @@
-﻿// Write your Javascript code.
+﻿var HistorySlider = function (containerID) {
+    var self = this,
+        $HistorySlider = $('#' + containerID),
+        $SliderThumnb = $HistorySlider.find('.HistorySlider_Thumb'),
+        $SliderTrack = $HistorySlider.find('.HistorySlider_Track'),
+        $SliderTrackFill = $HistorySlider.find('.HistorySlider_TrackFill'),
+        $ClickArea = $HistorySlider.find('.HistorySlider_ClickArea'),
+        $SliderPoints = $HistorySlider.find('.HistorySlider_Point');
+
+    this.value = 19;
+
+    /* helper to find slider value based on filled track width */
+    var findValueFromTrackFill = function (trackFillWidth) {
+        var totalWidth = $SliderTrack.width(),
+            onePercentWidth = totalWidth / 200,
+            value = Math.round((trackFillWidth / onePercentWidth) / 10);
+        return value;
+    }
+
+    /* change highlighted number based on new value */
+    var updateSelectedValue = function (newValue) {
+        $SliderPoints.removeClass('HistorySlider_PointActive');
+        $SliderPoints.eq(newValue).addClass('HistorySlider_PointActive');
+    }
+
+    /* highlight track based on new value (and move thumb) */
+    var updateHighlightedTrack = function (newPosition) {
+        newPosition = (newPosition / 2).toFixed(1).toString().split('.').join('') + '%';
+        $SliderTrackFill.css('width', newPosition);
+    }
+
+    /* set up drag funcationality for thumbnail */
+    var setupDrag = function ($element, initialXPosition) {
+        $SliderTrackFill.addClass('HistorySlider_TrackFill-stopAnimation');
+        var trackWidth = $SliderTrackFill.width();
+
+        var newValue = findValueFromTrackFill(trackWidth);
+        updateSelectedValue(newValue);
+
+        $element.on('mousemove', function (e) {
+            var newPosition = trackWidth + e.clientX - initialXPosition;
+            self.imitateNewValue(newPosition);
+
+            newValue = findValueFromTrackFill($SliderTrackFill.width());
+            updateSelectedValue(newValue);
+        });
+    }
+    /* remove drag functionality for thumbnail */
+    var finishDrag = function ($element) {
+        $SliderTrackFill.removeClass('HistorySlider_TrackFill-stopAnimation');
+        $element.off('mousemove');
+        var newValue = findValueFromTrackFill($SliderTrackFill.width());
+        self.updateSliderValue(newValue);
+    }
+
+    /* method to update all things required when slider value updates */
+    this.updateSliderValue = function (newValue) {
+        updateSelectedValue(newValue);
+        updateHighlightedTrack(newValue);
+        self.value = newValue;
+        console.log('this.value = ', self.value);
+    }
+
+    /* method to imitate new value without animation */
+    this.imitateNewValue = function (XPosition) {
+        $SliderTrackFill.addClass('HistorySlider_TrackFill-stopAnimation');
+        if (XPosition > $SliderTrack.width()) {
+            XPosition = $SliderTrack.width();
+        }
+        $SliderTrackFill.css('width', XPosition + 'px');
+    }
+
+    /*
+     * bind events when instance created
+     */
+    $ClickArea.on('mousedown', function (e) {
+        /* if a number or thumbnail has been clicked, use their event instead */
+        var $target = $(e.target);
+        if ($target.hasClass('HistorySlider_Thumb')) {
+            return false;
+        }
+        /* now we can continue based on where the clickable area was clicked */
+        self.imitateNewValue(e.offsetX);
+        setupDrag($(this), e.clientX);
+    });
+
+    $ClickArea.on('mouseup', function (e) {
+        console.log('"$ClickArea" calling "finishDrag"');
+        finishDrag($(this));
+    });
+
+    // update value when markers are clicked
+    $SliderPoints.on('mousedown', function (e) {
+        e.stopPropagation();
+        var XPos = $(this).position().left + ($(this).width() / 2);
+        self.imitateNewValue(XPos);
+        setupDrag($ClickArea, e.clientX);
+    });
+
+    // when thumbnail is clicked down, init the drag stuff
+    $SliderThumnb.on('mousedown', function (e) {
+        e.stopPropagation();
+        setupDrag($(this), e.clientX);
+    });
+
+    // when the thumbnail is released, remove the drag stuff
+    $SliderThumnb.on('mouseup', function (e) {
+        console.log('"$SliderThumnb" calling "finishDrag"');
+        finishDrag($(this));
+    });
+}
+
+var historySlider = new HistorySlider('HistorySlider');
